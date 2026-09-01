f448f06e48c3fb0329e0c6e521209406c2381ae0
SED4906 <sed4906birdie@gmail.com>
2026-09-01 15:05:28 -0700

    Clean up in prebuild_files

--- prebuild_files/bootc.sh
+++ prebuild_files/bootc.sh
@@ -10,5 +10,8 @@ cp -r /ctx/bootc /tmp/bootc
 abuild -F -C /tmp/bootc -r -s /tmp/srcdest -P /tmp/repodest
 apk add /tmp/repodest/tmp/$(abuild -A)/bootc-*.apk
 
+. /root/.config/abuild/abuild.conf
+rm /etc/apk/keys/$(basename $PACKAGER_PRIVKEY).pub
+rm -r /root/.config
 rm /etc/doas.conf
 apk del abuild doas
